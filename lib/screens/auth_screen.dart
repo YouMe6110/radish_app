637148b65c9af548fd731dcf071b337efc46d87d
YouMe6110 <ume0116@naver.com>
2022-07-01 14:00:05 +0900

    반응형 Intro, 테마, PageController 구현

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,17 +3,23 @@ import 'package:radish_app/screens/start/intro_page.dart';
 
 // 비머가드 false 리턴화면
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({Key? key}) : super(key: key);
+  // 페이지 컨트롤러 인스턴스
+  PageController _pageController = PageController();
+
+  AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: PageView(children: [
-        IntroPage(),
-        Container(color: Colors.accents[2],),
-        Container(color: Colors.accents[5],),
-      ],
+      body: PageView(
+        controller: _pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          IntroPage(_pageController),
+          Container(color: Colors.accents[2],),
+          Container(color: Colors.accents[5],),
+        ],
       ),
     );
   }
